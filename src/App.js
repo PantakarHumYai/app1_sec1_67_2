75d51edb4ec9c68555c702837a306d880b585ac7
PantakarnHumYai <152370448+PantakarHumYai@users.noreply.github.com>
2025-01-16 10:45:21 +0700

    add event-class.js and calaclator.js & Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
 import React from "react";
-import { Header,Content,Footer } from "./func-components";
-import './style.css'
-import Calender from "./class-components";
+import { Calculator2 } from "./calaclator";
 function App() {
   
-  return (
-  <>
-    <Header/>
-    <p><center><Calender/></center></p>
-      <Content/>
-        <Footer/>
-  </>
-  );
+  return <Calculator2/>
 }
 export default App;
