9cd935ca19a665699801d386cef62bdfc43e752b
PantakarnHumYai <152370448+PantakarHumYai@users.noreply.github.com>
2025-02-20 11:49:10 +0700

    Commit2 JubeJube

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from "react";
 import Router2 from "./router2";
 import Router3 from "./router3";
 import Router4 from "./router4";
+import FormSelectFile from "./form-select-file";
  
 function App() {
-  return <Router4/>
+  return <FormSelectFile/>
 }
  
 export default App;
